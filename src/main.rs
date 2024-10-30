c068e64706551e6e405c6d3d7cab8870168274b7
pxwjj <1272149266@qq.com>
2024-10-30 13:28:38 +0000

    first used clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clap::Parser;
+
+///Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Arge {
+    ///Name of the person to greet
+    #[arg(short, long)]
+    name: String,
+
+    ///Nubmer of time to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Arge::parse();
+
+    for _ in 0..args.count {
+        println!("hello {}", args.name);
+    }
 }
