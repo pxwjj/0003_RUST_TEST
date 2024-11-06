c7af6f2ae47cb329c5ac9c339c8a0119278af920
pxwjj <1272149266@qq.com>
2024-11-06 08:27:01 +0000

    元组、数组、语句、表达式 and function

--- guess_num/src/main.rs
+++ guess_num/src/main.rs
@@ -1,6 +1,14 @@
 use rand::Rng;
 use std::{io,cmp::Ordering};
 
+fn print_hour (num: u32, ocloc: char) {
+    println!("how time? {num}{ocloc}");
+}
+
+fn num1_plus_num2 (num1: u32, num2: u32) -> u32 {
+    num1 + num2  // don`t use ';' 否则 就是语句了，不是表达式了。
+}
+
 fn main (){
     print!("please input a number between 0 to 100:\n");
 
@@ -40,4 +48,25 @@ fn main (){
             },
         };
     }
+
+    // 元组 - tuple
+    let t: (i32, f64, bool) = (2, 0.1, true);
+    let (x, y, z) = t;
+    println!("y = {y}"); // 0.1
+    println!("t.0 = {}, t.1 = {}, t.2 = {}", t.0, t.1, t.2); // 2, 0.1, true
+
+    // 数组 - array
+    let a: [i32; 4] = [1, 2, 3, 4];
+    println!("a[0] = {}", a[0]); // 1
+
+    print_hour(2, 'h');
+    println!("1+1 = {}", num1_plus_num2(1, 1));
+
+    // 表达式
+    let wpx = {
+        let wrj: u32 = 1;
+        wrj + 1 // 如果加了‘；’那么返回值就是 () 了。语句的返回值，默认就是这个玩意儿
+    };
+
+    println!("wpx = {wpx}");
 }
